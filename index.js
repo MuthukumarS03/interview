3197099b7035c7a744cca199a1a4c825bb0d9772
Muthukumar Sudalaiyandi <msudalaiyandi@paypal.com>
2016-09-24 15:12:09 -0700

    Added BinarySearchTree

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 var beautify = require('./src/util/beautify'),
 	testLinkedList = require('./src/datastructures/linkedList/test'),
-	testSort = require('./src/datastructures/sort/test');;
+	testSort = require('./src/datastructures/sort/test'),
+	testTree = require('./src/datastructures/tree/test');
 
 /** Linked List **/
 testLinkedList();
 
 /** Sort **/
 testSort();
+
+/** Tree **/
+testTree();
