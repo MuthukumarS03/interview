6382e960344d74e944fd8a48dbcd16385efbdba3
Muthukumar Sudalaiyandi <msudalaiyandi@paypal.com>
2016-09-24 06:51:01 -0700

    Quick sort added

--- src/datastructures/sort/swap.js
+++ src/datastructures/sort/swap.js
@@ -1,6 +1,9 @@
 'use strict';
 
 module.exports = function swap(arr, i, j) {
+	if(i === j) {
+		return;
+	}
     console.log('Swapping ', arr[i], arr[j]);
     arr[i] = arr[i] + arr[j];
     arr[j] = arr[i] - arr[j];
--- src/datastructures/sort/test.js
+++ src/datastructures/sort/test.js
@@ -2,13 +2,18 @@
 var beautify = require('../../util/beautify'),
     bubbleSort = require('./bubbleSort'),
     selectionSort = require('./selectionSort'),
-    mergeSort = require('./mergeSort');
+    mergeSort = require('./mergeSort'),
+    quickSort = require('./quickSort');
 
 
 function testSorting(sort) {
 
     function sorting () {
         var arr = [ 22, -32, 5, -34, 5];
+        // var arr = [ 4, 4, 4, 4];
+        // var arr = [ 4, 6, 3, 2, 1, 9, 7];
+        // var arr = [ 35, 33, 42, 10, 14, 19, 27, 44, 26, 31];
+
         console.log('Array before sort ', arr);
         sort(arr);
         console.log('Array after sort ', arr);
@@ -26,10 +31,13 @@ module.exports = function testSort() {
     /** Bubble Sort **/
     beautify(testSorting(bubbleSort));
 
-    /** Selection Sort **/
+    // /** Selection Sort **/
     beautify(testSorting(selectionSort));
 
-    /** Merge Sort **/
+    // /** Merge Sort **/
     beautify(testSorting(mergeSort));
+
+    /** Quick Sort **/
+    beautify(testSorting(quickSort));
 
 };
